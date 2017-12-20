271dc3b8dcf649cae9cbb43a09ddf170e0524543
oli <oli@dev.com>
2017-12-20 17:02:42 +0800

    first update

---
+++ res/js/config.js
@@ -0,0 +1,28 @@
+requirejs.config({
+    baseUrl: '../res/js',
+    paths: {
+        jquery	    : 'vendor/jquery/jquery.min',
+        bs		    : 'vendor/bootstrap/js/bootstrap.min',
+        metisMenu   : 'vendor/metisMenu/metisMenu.min',
+        sbAdmin2    : 'vendor/sb-admin2/js/sb-admin-2.min',
+        vue		    : 'vendor/vue/vue.min',
+    },
+    shim: {
+        bs: {
+            deps: [
+                'jquery',
+            ]
+        },
+        metisMenu: {
+            deps: [
+                'jquery',
+            ]
+        },
+        sbAdmin2: {
+            deps: [
+                'bs',
+                'metisMenu',
+            ]
+        }
+    }
+});
